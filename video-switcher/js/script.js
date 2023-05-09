74f414ca677bb591288ef1a67fb8cbee9072559c
Hasan <hasan1117@gmail.com>
2023-05-09 12:27:29 -0700

    fixed issue with webcam

--- video-switcher/js/script.js
+++ video-switcher/js/script.js
@@ -6,24 +6,28 @@ var video = document.getElementById("myVideo");
 document.addEventListener("keypress", function(event) {
     if (event.keyCode == 49) { //If you Press 1, what happens
         
-       
-        video.srcObject = "videos/sample-5s.mp4";
+       video.srcObject = null;
+
+        video.src = "videos/sample-5s.mp4";
         video.play() 
 
     }
     if(event.keyCode == 50) {
         
+        video.srcObject = null;
 
         video.src = "videos/The Land of Chocolate - Simpsons Clip.mp4"
 
     }
 
     if(event.keyCode == 51) {
+        video.srcObject = null;
 
         video.src = "videos/sample-20s.mp4"
     }
 
     if(event.keyCode == 52) {
+        video.srcObject = null;
 
         video.src = "videos/sample-10s.mp4"
     }
@@ -32,9 +36,7 @@ document.addEventListener("keypress", function(event) {
         window.navigator.mediaDevices.getUserMedia({ video: true })
         .then(stream => {
             video.srcObject = stream;
-            video.onloadedmetadata = (e) => {
-                video.play();
-            };
+           video.play()
         })
     }
 
