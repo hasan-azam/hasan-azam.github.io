13d317d906414a6bcd462d2e56f8db748f46585e
Hasan <hasan1117@gmail.com>
2023-05-09 12:19:53 -0700

    Implemented basic video

--- video-switcher/js/script.js
+++ video-switcher/js/script.js
@@ -5,20 +5,37 @@ var video = document.getElementById("myVideo");
 
 document.addEventListener("keypress", function(event) {
     if (event.keyCode == 49) { //If you Press 1, what happens
-        video.src = "videos/sample-5s.mp4";  
-        title = "1"
+        
+       
+        video.srcObject = "videos/sample-5s.mp4";
+        video.play() 
 
     }
     if(event.keyCode == 50) {
+        
+
         video.src = "videos/The Land of Chocolate - Simpsons Clip.mp4"
+
     }
 
     if(event.keyCode == 51) {
+
         video.src = "videos/sample-20s.mp4"
     }
 
     if(event.keyCode == 52) {
+
         video.src = "videos/sample-10s.mp4"
+    }
+
+    if(event.keyCode == 53) {
+        window.navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            video.srcObject = stream;
+            video.onloadedmetadata = (e) => {
+                video.play();
+            };
+        })
     }
 
   });
