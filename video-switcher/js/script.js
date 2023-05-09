e3c6aa62a6f0de2e7ab350ddb12adef2c6059fab
Hasan <hasan1117@gmail.com>
2023-05-09 11:34:51 -0700

    Video Switcher Update

---
+++ video-switcher/js/script.js
@@ -0,0 +1,16 @@
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+
+document.addEventListener("keypress", function(event) {
+    if (event.keyCode == 49) { //If you Press 1, what happens
+        video.src = "/videos/sample-5s.mp4";  
+        title = "1"
+
+    }
+    if(event.keyCode == 50) {
+        video.src = "/videos/The Land of Chocolate - Simpsons Clip.mp4"
+    }
+  });
+
