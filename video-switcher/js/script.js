cb08ef71e509ff907eba55e5a71b24704f8a0ce3
Hasan <hasan1117@gmail.com>
2023-05-09 11:46:43 -0700

    Got basic switcher with test videos working

--- video-switcher/js/script.js
+++ video-switcher/js/script.js
@@ -5,12 +5,21 @@ var video = document.getElementById("myVideo");
 
 document.addEventListener("keypress", function(event) {
     if (event.keyCode == 49) { //If you Press 1, what happens
-        video.src = "/videos/sample-5s.mp4";  
+        video.src = "videos/sample-5s.mp4";  
         title = "1"
 
     }
     if(event.keyCode == 50) {
-        video.src = "/videos/The Land of Chocolate - Simpsons Clip.mp4"
+        video.src = "videos/The Land of Chocolate - Simpsons Clip.mp4"
     }
+
+    if(event.keyCode == 51) {
+        video.src = "videos/sample-20s.mp4"
+    }
+
+    if(event.keyCode == 52) {
+        video.src = "videos/sample-10s.mp4"
+    }
+
   });
 
