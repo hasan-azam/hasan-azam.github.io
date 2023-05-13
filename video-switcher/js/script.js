413e70f63d07d302c45aacab3eec65f8bb362c60
Hasan <hasan1117@gmail.com>
2023-05-13 16:34:40 -0700

    Shifted flower vid, deleted old ones

--- video-switcher/js/script.js
+++ video-switcher/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener("keypress", function(event) {
         
         video.srcObject = null;
 
-        video.src = "videos/flower-4x3.mp4"
+        video.src = "videos/2-flower.mp4"
 
     }
 
